4d87662f83bbf334c9a807f28b8bd710fb0ffcdf
Eli Fatsi <eli.fatsi@viget.com>
2014-11-05 15:54:11 -0700

    Must provide username before chatting

--- priv/static/js/room.js
+++ priv/static/js/room.js
@@ -2,11 +2,13 @@ $(function(){
   var socket = new Phoenix.Socket("ws://" + location.host + "/ws");
   var height;
 
-  var $messages = $("#messages");
-  var $form     = $("#chat");
-  var $username = $("#username");
-  var $message  = $("#message");
-  var $roomName = $("#room_info").data("name")
+  var $messages     = $("#messages");
+  var $roomName     = $("#room_info").data("name")
+
+  var $usernameForm = $("#username-form");
+  var $username     = $("#username");
+  var $messageForm  = $("#message-form");
+  var $message      = $("#message");
 
   var scrollToBottom = function() {
     height = $messages[0].scrollHeight;
@@ -16,10 +18,20 @@ $(function(){
   scrollToBottom();
 
   socket.join("rooms", $roomName, {}, function(channel) {
-    $form.submit(function(e) {
+    $usernameForm.submit(function(e) {
+      e.preventDefault();
+      var token = Math.random().toString(36).substr(8);
+
+      channel.send("user:new", {username: $username.val(), token: token});
+
+      $usernameForm.hide();
+      $messageForm.show();
+    })
+
+    $messageForm.submit(function(e) {
       e.preventDefault();
 
-      channel.send("message:new", {username: $username.val(), body: $message.val()});
+      channel.send("message:new", {body: $message.val()});
       $message.val("");
     })
 
@@ -29,12 +41,12 @@ $(function(){
     });
 
     channel.on("user:entered", function(message) {
-      $messages.append("<br/>someone entered")
+      $messages.append("<br/>[" + message.username + "] entered")
       scrollToBottom();
     });
 
     channel.on("user:left", function(message) {
-      $messages.append("<br/>someone left")
+      $messages.append("<br/>[" + message.username + "] left")
       scrollToBottom();
     });
   });
--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -3,27 +3,58 @@ defmodule Pitch.RoomChannel do
   import Ecto.Query, only: [from: 2]
 
   def join(socket, _topic, _message) do
-    broadcast socket, "user:entered", %{}
     {:ok, socket}
   end
 
   def leave(socket, _message) do
-    broadcast socket, "user:left", %{}
+    if user = get_assign(socket, :user) do
+      Repo.delete user
+      broadcast socket, "user:left", %{username: user.name}
+    end
+
+    socket
+  end
+
+  def event(socket, "user:new", message) do
+    room = room_from_socket(socket)
+    user = create_user(room, message)
+
+    socket = assign(socket, :user, user)
+    socket = assign(socket, :room, room)
+
+    broadcast socket, "user:entered", %{username: user.name}
     socket
   end
 
   def event(socket, "message:new", message) do
-    save_message(socket, message)
-    broadcast socket, "message:new", %{username: message["username"], body: message["body"]}
+    room    = get_assign(socket, :room)
+    user    = get_assign(socket, :user)
+    message = save_message(room, user, message)
+
+    broadcast socket, "message:new", %{username: user.name, body: message.body}
     socket
   end
 
-  defp save_message(socket, message) do
-    [room] = Repo.all(from r in Room, where: r.name == ^socket.topic)
+  # private
+
+  defp create_user(room, message) do
+    Repo.insert %User{
+      room_id: room.id,
+      name:    message["username"],
+      token:   message["token"]
+    }
+  end
+
+  defp save_message(room, user, message) do
     Repo.insert %Message{
       room_id:  room.id,
-      username: message["username"],
+      username: user.name,
       body:     message["body"]
     }
+  end
+
+  defp room_from_socket(socket) do
+    Repo.all(from r in Room, where: r.name == ^socket.topic)
+    |> List.first
   end
 end
