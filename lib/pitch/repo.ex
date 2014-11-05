4cc3286003e2442d541676462d06041bfff94e36
Eli Fatsi <eli.fatsi@viget.com>
2014-11-05 08:17:58 -0700

    Updated postgres credentials

--- lib/pitch/repo.ex
+++ lib/pitch/repo.ex
@@ -2,7 +2,7 @@ defmodule Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres
 
   def conf do
-    parse_url "ecto://efatsi:postgres@localhost/pitch"
+    parse_url "ecto://elixir_pitch:L4yZL2OumjNufJEz@localhost/pitch"
   end
 
   def priv do
