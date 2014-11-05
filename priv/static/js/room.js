5cc7c00328f4cd3d36e2d3cd84ea18113a7f9f4e
Eli Fatsi <eli.fatsi@viget.com>
2014-11-05 14:07:24 -0700

    Scrolling to bottom of chat window on any action

--- priv/static/js/room.js
+++ priv/static/js/room.js
@@ -1,11 +1,19 @@
 $(function(){
   var socket = new Phoenix.Socket("ws://" + location.host + "/ws");
+  var height;
 
   var $messages = $("#messages");
   var $form     = $("#chat");
   var $username = $("#username");
   var $message  = $("#message");
   var $roomName = $("#room_info").data("name")
+
+  var scrollToBottom = function() {
+    height = $messages[0].scrollHeight;
+    $messages.scrollTop(height);
+  }
+
+  scrollToBottom();
 
   socket.join("rooms", $roomName, {}, function(channel) {
     $form.submit(function(e) {
@@ -17,17 +25,17 @@ $(function(){
 
     channel.on("message:new", function(message) {
       $messages.append("<br/>[" + message.username + "] " + message.body)
-
-      var height = $messages[0].scrollHeight;
-      $messages.scrollTop(height);
+      scrollToBottom();
     });
 
     channel.on("user:entered", function(message) {
       $messages.append("<br/>someone entered")
+      scrollToBottom();
     });
 
     channel.on("user:left", function(message) {
       $messages.append("<br/>someone left")
+      scrollToBottom();
     });
   });
 });
